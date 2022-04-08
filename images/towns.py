6f1e40067d3b13456fa9b4bb82d79f197e624129
imac <sldifsdkfyb@mail.com>
2022-04-08 09:17:37 +0300

    Towns information added

---
+++ images/towns.py
@@ -0,0 +1,17 @@
+towns = {
+    "Москва": [
+        "/Moscow/moscow1.jpeg",
+        "/Moscow/moscow2.jpeg",
+        "/Moscow/moscow3.jpeg",
+    ],
+    "Париж": [
+        "/Paris/paris1.jpeg",
+        "/Paris/paris2.jpeg",
+        "/Paris/paris3.jpeg",
+    ],
+    "Смоленск": [
+        "/Smolensk/smolensk1.jpeg",
+        "/Smolensk/smolensk2.jpeg",
+        "/Smolensk/smolensk3.jpeg",
+    ]
+}
