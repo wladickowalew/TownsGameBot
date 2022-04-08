3d337263eed467e84af755f0ac6eb8465b864497
imac <sldifsdkfyb@mail.com>
2022-04-08 09:59:16 +0300

    hot fix

--- objects.py
+++ objects.py
@@ -7,11 +7,11 @@ users = {}
 class User:
     def __init__(self, user_id):
         self.id = user_id
-        self.current_town = choice(towns.keys())
+        self.current_town = choice(list(towns.keys()))
         self.photos = towns[self.current_town].copy()
         users[self.id] = self
 
     def get_photo(self):
         if not self.photos:
             return
-        return self.photos.pop(randint(0, len(self.photos) - 1))
+        return "images" + self.photos.pop(randint(0, len(self.photos) - 1))
